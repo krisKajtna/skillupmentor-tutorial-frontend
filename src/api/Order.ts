fc4f89f31cc0ddff715434332a52018b30e9719c
Kris <kris.kajtna@gmail.com>
2025-05-20 18:54:17 +0200

    chart

--- src/api/Order.ts
+++ src/api/Order.ts
@@ -3,7 +3,7 @@ import { apiRequest } from './Api'
 import { OrderType } from 'models/order'
 
 export const fetchChart = async () =>
-  apiRequest<undefined, { data: string; sum: string }[]>(
+  apiRequest<undefined, { date: string; sum: string }[]>(
     'get',
     `${apiRoutes.ORDERS_PREFIX}/chart`,
   )
